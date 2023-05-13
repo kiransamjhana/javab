d70d25f79bb0132d5b9f9112e77c6a154db72f56
kiransamjhana <kiranmanandhar956@gmail.com>
2023-05-13 11:53:09 +1000

    new update

--- script.js
+++ script.js
@@ -1,34 +1,96 @@
-const fruits = [
-  { name: "apple", price: 200, status: "avilable" },
-  { name: "orange", price: 300, status: "Unavilable" },
-  { name: "kiwi", price: 400, status: "avilable" },
-];
+// const fruits = [
+//   { name: "apple", price: 200, status: "avilable" },
+//   { name: "orange", price: 300, status: "Unavilable" },
+//   { name: "kiwi", price: 400, status: "avilable" },
+// ];
 
-console.table(fruits);
-fruits.pop();
-fruits.unshift("kiwi");
-fruits.shift();
-fruits.push("apple");
-fruits.slice(0, 1);
-console.table(fruits);
+// console.table(fruits);
+// fruits.pop();
+// fruits.unshift("kiwi");
+// fruits.shift();
+// fruits.push("apple");
+// fruits.slice(0, 1);
+// console.table(fruits);
 
-const person = [1, 2, 3, 4, 5];
-console.log(person);
+// const person = [1, 2, 3, 4, 5];
+// console.log(person);
 
-// const division = document.getElementsByClassName("kiran");
+// // const division = document.getElementsByClassName("kiran");
 
-// division[2].style.backgroundColor = "red";
+// // division[2].style.backgroundColor = "red";
 
-let uniquN = [];
+// let uniquN = [];
 
-for (let i = 0; i < 50; i++) {
-  if (i % 2) {
-    uniquN.push;
+// for (let i = 0; i < 50; i++) {
+//   if (i % 2) {
+//     uniquN.push;
+//   }
+// }
+// console.log(uniquN);
+
+// const country = ["nepal", "india", "china", "pakistan"];
+// const val = Array.country;
+// console.log(val);
+// console.log(country);
+
+// Interpreters vs compilers
+// Call stack and memory heap
+// Javascript Runtime environment
+// Object-oriented Programming (OOP):
+
+// Functional Programming (FP):
+
+// DOM Manipulation with Project 1
+
+// const a = {
+//   //properties
+//   name: "kiran",
+//   sing: () => {
+//     console.log("Kiran can sing");
+//   },
+// }; // literal
+// console.log(a.sing());
+
+// const person = () => {
+//   const bio = () => {
+//     return " has a height of aand likes to sing";
+//   };
+//   return bio;
+// };
+// const per = person();
+// console.log(per());
+// // const b= new Object();//constructort
+
+// Class in Javascript
+
+// First crate the oject in the class
+
+// class Fruit {
+//   constructor() {
+//     console.log("I am inside the constructor");
+//   }
+//   bio() {
+//     return " inside the biog";
+//   }
+//   si;
+// }
+
+// const obj = new Fruit();
+// // console.log(typeof Fruit);
+// // console.log(typeof Fruit);
+
+// This keyword
+
+class Fruit {
+  bio() {
+    console.log(this);
   }
 }
-console.log(uniquN);
 
-const country = ["nepal", "india", "china", "pakistan"];
-const val = Array.country;
-console.log(val);
-console.log(country);
+const obj = new Fruit();
+console.timeLog(obj.bio());
+// function a() {
+//   console.log(this);
+// }
+
+// a();
