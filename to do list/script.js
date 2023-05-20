0cf89a34bac8814b37d02b03c99e3318aecc7d03
kiransamjhana <kiranmanandhar956@gmail.com>
2023-05-20 15:51:55 +1000

    new file

--- to do list/script.js
+++ to do list/script.js
@@ -1,5 +1,63 @@
-const addElement = document.getElementsByClassName("form-control");
-// console.log(addElement);
-document.getElementsByClassName("form-control").innerText = addElement;
+// capture the tdata  from the form on form submit
 
-console.log(addElement);
+let taskList = [];
+
+const entryElm = document.getElementById("entry");
+
+const handlOnSubmit = (e) => {
+  const formDt = new FormData(e);
+  const task = formDt.get("task");
+  const hr = formDt.get("hr");
+
+  console.log(task, hr);
+
+  const taskObj = {
+    task,
+    hr,
+    id: randomGennerator(),
+  };
+
+  taskList.push(taskObj);
+  console.log(taskList);
+  displayTask();
+};
+
+// DISPLAYIG DATA IN THE BROWWER
+
+const displayTask = () => {
+  let str = "";
+  taskList.map((item, i) => {
+    str += `<tr>
+    <td>${item.task}</td>
+    <td>${item.task}</td>
+    <td>
+      <button class="btn btn-danger" onclick="deleteTask('${item.id}')">
+        <i class="fa-solid fa-trash"></i>
+      </button>
+      <button class="btn btn-success">
+        <i class="fa-solid fa-arrow-left"></i>
+      </button>
+    </td>
+  </tr>`;
+  });
+  entryElm.innerHTML = str;
+};
+
+//  create unique id
+
+const randomGennerator = (length = 6) => {
+  const collection = "qwertyuiopasdfghjklzxcvbnm";
+  let str = "";
+  for (let i = 0; i < length; i++) {
+    const ranNum = Math.round(Math.random() * collection.length - 1);
+    str += collection[ranNum];
+  }
+  return str;
+};
+
+const deleteTask = (id) => {
+  if (window.confirm("Are you sure you wanat to delete it ")) {
+    taskList = taskList.filter((item) => item.id !== id);
+    displayTask();
+  }
+};
